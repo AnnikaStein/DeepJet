fix: path to DJC container feat: AUC calculation feat: prediction with or without attack

pytorch/predict_pytorch.py
@@ -11,9 +11,15 @@ parser.add_argument("-b", help="batch size, overrides the batch size from the tr
 parser.add_argument("--gpu",  help="select specific GPU", metavar="OPT", default="")
 parser.add_argument("--unbuffered", help="do not read input in memory buffered mode (for lower memory consumption on fast disks)", default=False, action="store_true")
 parser.add_argument("--pad_rowsplits", help="pad the row splits if the input is ragged", default=False, action="store_true")
+parser.add_argument("--attack", help="use adversarial attack (Noise|FGSM) or leave blank to use undisturbed features only", default="", action="store_true")
+parser.add_argument("--att_magnitude", help="distort input features with adversarial attack, using specified magnitude of attack", default="-1", action="store_true")
+parser.add_argument("--restrict_impact", help="limit attack impact to this fraction of the input value (percent-cap on distortion)", default="-1", action="store_true")
 
 args = parser.parse_args()
 batchsize = int(args.b)
+attack = args.attack
+att_magnitude = float(args.FGSM_epsilon)
+restrict_impact = float(args.restrict_impact)
 
 import imp
 import numpy as np
@@ -33,7 +39,11 @@ from tqdm import tqdm
 inputdatafiles=[]
 inputdir=None
 
-def test_loop(dataloader, model, nbatches, pbar):
+def cross_entropy_one_hot(input, target):
+    _, labels = target.max(dim=1)
+    return nn.CrossEntropyLoss()(input, labels)
+
+def test_loop(dataloader, model, nbatches, pbar, attack = "", att_magnitude = -1., restrict_impact = -1., loss_fn = cross_entropy_one_hot):
     predictions = 0
     
     with torch.no_grad():
@@ -46,6 +56,48 @@ def test_loop(dataloader, model, nbatches, pbar):
             vtx = torch.Tensor(features_list[3]).to(device)
             #pxl = torch.Tensor(features_list[4]).to(device)
             y = torch.Tensor(truth_list[0]).to(device)    
+            
+            
+            
+            if attack == 'Noise':
+                #print('Do Noise')
+                glob = apply_noise(glob, 
+                                   magn=att_magnitude,
+                                   offset=[0],
+                                   dev=device,
+                                   restrict_impact=restrict_impact,
+                                   var_group='glob')
+                cpf = apply_noise(cpf, 
+                                   magn=att_magnitude,
+                                   offset=[0],
+                                   dev=device,
+                                   restrict_impact=restrict_impact,
+                                   var_group='cpf')
+                npf = apply_noise(npf, 
+                                   magn=att_magnitude,
+                                   offset=[0],
+                                   dev=device,
+                                   restrict_impact=restrict_impact,
+                                   var_group='npf')
+                vtx = apply_noise(vtx, 
+                                   magn=att_magnitude,
+                                   offset=[0],
+                                   dev=device,
+                                   restrict_impact=restrict_impact,
+                                   var_group='vtx')
+
+            elif attack == 'FGSM':
+                #print('Do FGSM')
+                glob, cpf, npf, vtx = fgsm_attack(sample=(glob,cpf,npf,vtx), 
+                                                   epsilon=att_magnitude,
+                                                   dev=device,
+                                                   targets=y,
+                                                   thismodel=model,
+                                                   thiscriterion=loss_fn,
+                                                   restrict_impact=restrict_impact)
+            
+            
+            
             # Compute prediction
             pred = nn.Softmax(dim=1)(model(glob,cpf,npf,vtx)).cpu().numpy()
             if b == 0:
@@ -137,7 +189,7 @@ for inputfile in inputdatafiles:
     with tqdm(total = gen.getNBatches()) as pbar:
         pbar.set_description('Predicting : ')
     
-    predicted = test_loop(gen.feedNumpyData(), model, nbatches=gen.getNBatches(), pbar = pbar)
+    predicted = test_loop(gen.feedNumpyData(), model, nbatches=gen.getNBatches(), pbar = pbar, attack = attack, att_magnitude = att_magnitude, restrict_impact = restrict_impact)
     
     x = td.transferFeatureListToNumpy(args.pad_rowsplits)
     w = td.transferWeightListToNumpy(args.pad_rowsplits)

scripts/plot_roc.py
@@ -2,7 +2,7 @@ print("start import")
 import os
 import matplotlib
 matplotlib.use('Agg')
-from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.metrics import roc_curve, roc_auc_score, auc
 from scipy.interpolate import InterpolatedUnivariateSpline
 from pdb import set_trace
 from itertools import chain
@@ -29,19 +29,23 @@ def spit_out_roc(disc,truth_array,selection_array):
     clean = coords.drop_duplicates(subset=['fpr'])
     spline = InterpolatedUnivariateSpline(clean.fpr, clean.tpr,k=1)
     tprs = spline(newx)
+    print('AUC: ', str(auc(clean.fpr,clean.tpr)))
     return tprs, newx
 
 
 
 pred = []
-isDeepJet = False
+isDeepJet = True
 if isDeepJet:
     listbranch = ['prob_isB', 'prob_isBB','prob_isLeptB', 'prob_isC','prob_isUDS','prob_isG','isB', 'isBB', 'isLeptB', 'isC','isUDS','isG','jet_pt', 'jet_eta']
 else:
     listbranch = ['prob_isB', 'prob_isBB', 'prob_isC','prob_isUDSG','isB', 'isBB', 'isC','isUDSG','jet_pt', 'jet_eta']
 
-dirz = '/data/ml/ebols/DeepCSV_PredictionsTest/'
+#some_path = '/eos/user/a/anstein/DeepJet/Train_DF/nominal_with_etarel_phirel/predict/pred_ntuple_merged_342.root'
+    
+dirz = '/eos/user/a/anstein/DeepJet/Train_DF/adversarial_with_etarel_phirel/predict/'
 truthfile = open( dirz+'outfiles.txt','r')
+#truthfile = open( dirz+'one_prediction.txt','r')
 print("opened text file")
 rfile1 = ROOT.TChain("tree")
 count = 0
@@ -70,7 +74,7 @@ else:
     veto_udsg = (df['isUDSG'] != 1) & ( df['jet_pt'] > 30) & (summed_truth != 0)
 
 
-f = ROOT.TFile("ROCS_DeepCSV.root", "recreate")
+f = ROOT.TFile("ROCS_DeepJet_adversarial_medium.root", "recreate")
 
 x1, y1 = spit_out_roc(disc,b_jets,veto_c)
 x2, y2 = spit_out_roc(disc,b_jets,veto_udsg)