fix: loss and accuracy calculation; adversarial training

ParT/ParT.py
@@ -256,24 +256,27 @@ class PairEmbed(nn.Module):
 
     def forward(self, x):
         # x: (batch, v_dim, seq_len)
-        with torch.no_grad():
-            batch_size, _, seq_len = x.size()
-            if not self.for_onnx:
-                i, j = torch.tril_indices(seq_len, seq_len, offset = -1, device=x.device)
-                x = x.unsqueeze(-1).repeat(1, 1, 1, seq_len)
-                xi = x[:, :, i, j]  # (batch, dim, seq_len*(seq_len+1)/2)
-                xj = x[:, :, j, i]
-                #k = (i != j)*1
-                x = self.pairwise_lv_fts(xi, xj)
-            else:
-                i, j = tril_indices(x, seq_len, offset = True)
-                x = x.unsqueeze(-1).repeat(1, 1, 1, seq_len)
-                xi = x[:, :, i, j]  # (batch, dim, seq_len*(seq_len+1)/2)
-                xj = x[:, :, j, i]
-                #k = (i != j)*1
-                x = self.pairwise_lv_fts(xi, xj)
-                #x = self.pairwise_lv_fts(x.unsqueeze(-1), x.unsqueeze(-2)).view(batch_size, -1, seq_len * seq_len)
-
+        
+        # allow gradient computation for adversarial training here ?!
+#        with torch.no_grad():
+# START indentation if doing nominal training
+        batch_size, _, seq_len = x.size()
+        if not self.for_onnx:
+            i, j = torch.tril_indices(seq_len, seq_len, offset = -1, device=x.device)
+            x = x.unsqueeze(-1).repeat(1, 1, 1, seq_len)
+            xi = x[:, :, i, j]  # (batch, dim, seq_len*(seq_len+1)/2)
+            xj = x[:, :, j, i]
+            #k = (i != j)*1
+            x = self.pairwise_lv_fts(xi, xj)
+        else:
+            i, j = tril_indices(x, seq_len, offset = True)
+            x = x.unsqueeze(-1).repeat(1, 1, 1, seq_len)
+            xi = x[:, :, i, j]  # (batch, dim, seq_len*(seq_len+1)/2)
+            xj = x[:, :, j, i]
+            #k = (i != j)*1
+            x = self.pairwise_lv_fts(xi, xj)
+            #x = self.pairwise_lv_fts(x.unsqueeze(-1), x.unsqueeze(-2)).view(batch_size, -1, seq_len * seq_len)
+# END indentation if doing nominal training
         elements = self.embed(x)  # (batch, embed_dim, num_elements
         
         if not self.for_onnx:

ParT/attacks_ParT.py
@@ -70,9 +70,10 @@ def fgsm_attack(epsilon=1e-2,sample=None,targets=None,thismodel=None,thiscriteri
     xadv_cpf.requires_grad = True
     xadv_npf.requires_grad = True
     xadv_vtx.requires_grad = True
-    #xadv_cpf_pts.requires_grad = True
-    #xadv_npf_pts.requires_grad = True
-    #xadv_vtx_pts.requires_grad = True
+    xadv_cpf_pts.requires_grad = True
+    xadv_npf_pts.requires_grad = True
+    xadv_vtx_pts.requires_grad = True
+    #print(xadv_cpf_pts.size()) # bsize, n_cpf_pts, n_cpf_pts_features turned out that there are 10 instead of 4 features
 
     #new_inpts = (xadv_glob,
     #             xadv_cpf,xadv_npf,xadv_vtx,
@@ -84,6 +85,7 @@ def fgsm_attack(epsilon=1e-2,sample=None,targets=None,thismodel=None,thiscriteri
     loss = thiscriterion(preds, targets)
 
     thismodel.zero_grad()
+    #loss.backward(retain_variables=True)
     loss.backward()
 
     with torch.no_grad():
@@ -91,17 +93,18 @@ def fgsm_attack(epsilon=1e-2,sample=None,targets=None,thismodel=None,thiscriteri
         dx_cpf = torch.sign(xadv_cpf.grad.detach())
         dx_npf = torch.sign(xadv_npf.grad.detach())
         dx_vtx = torch.sign(xadv_vtx.grad.detach())
-        #dx_cpf_pts = torch.sign(xadv_cpf_pts.grad.detach())
-        #dx_npf_pts = torch.sign(xadv_npf_pts.grad.detach())
-        #dx_vtx_pts = torch.sign(xadv_vtx_pts.grad.detach())
+        dx_cpf_pts = torch.sign(xadv_cpf_pts.grad.detach())
+        dx_npf_pts = torch.sign(xadv_npf_pts.grad.detach())
+        dx_vtx_pts = torch.sign(xadv_vtx_pts.grad.detach())
+        #print(dx_cpf_pts.size())
 
         #xadv_glob += epsilon * epsilon_factors['glob'] * dx_glob
         xadv_cpf += epsilon * epsilon_factors['cpf'] * dx_cpf
         xadv_npf += epsilon * epsilon_factors['npf'] * dx_npf
         xadv_vtx += epsilon * epsilon_factors['vtx'] * dx_vtx
-        #xadv_cpf_pts += epsilon * epsilon_factors['cpf_pts'] * dx_cpf_pts
-        #xadv_npf_pts += epsilon * epsilon_factors['npf_pts'] * dx_npf_pts
-        #xadv_vtx_pts += epsilon * epsilon_factors['vtx_pts'] * dx_vtx_pts
+        xadv_cpf_pts += epsilon * epsilon_factors['cpf_pts'] * dx_cpf_pts
+        xadv_npf_pts += epsilon * epsilon_factors['npf_pts'] * dx_npf_pts
+        xadv_vtx_pts += epsilon * epsilon_factors['vtx_pts'] * dx_vtx_pts
 
         if reduced:
             #for i in range(vars_per_candidate['glob']):
@@ -172,55 +175,55 @@ def fgsm_attack(epsilon=1e-2,sample=None,targets=None,thismodel=None,thiscriteri
                                 xadv_vtx[high_impact,j,i] = vtx[high_impact,j,i] + allowed_perturbation[high_impact] * dx_vtx[high_impact,j,i] 
 ### NEW
 
-            #for j in range(cands_per_variable['cpf_pts']):
-            #    for i in range(vars_per_candidate['cpf_pts']):
-            #        if i in integer_variables_by_candidate['cpf_pts']:
-            #            xadv_cpf_pts[:,j,i] = cpf_pts[:,j,i]
-            #        else:
-            #            defaults_cpf_pts = cpf_pts[:,j,i].cpu() == defaults_per_variable['cpf_pts'][i]
-            #            if torch.sum(defaults_cpf_pts) != 0:
-            #                xadv_cpf_pts[:,j,i][defaults_cpf_pts] = cpf_pts[:,j,i][defaults_cpf_pts]
+            for j in range(cands_per_variable['cpf_pts']):
+                for i in range(vars_per_candidate['cpf_pts']):
+                    if i in integer_variables_by_candidate['cpf_pts']:
+                        xadv_cpf_pts[:,j,i] = cpf_pts[:,j,i]
+                    else:
+                        defaults_cpf_pts = cpf_pts[:,j,i].cpu() == defaults_per_variable['cpf_pts'][i]
+                        if torch.sum(defaults_cpf_pts) != 0:
+                            xadv_cpf_pts[:,j,i][defaults_cpf_pts] = cpf_pts[:,j,i][defaults_cpf_pts]
 
-            #            if restrict_impact > 0:
-            #                difference = xadv_cpf_pts[:,j,i] - cpf_pts[:,j,i]
-            #                allowed_perturbation = restrict_impact * torch.abs(cpf_pts[:,j,i])
-            #                high_impact = torch.abs(difference) > allowed_perturbation
+                        if restrict_impact > 0:
+                            difference = xadv_cpf_pts[:,j,i] - cpf_pts[:,j,i]
+                            allowed_perturbation = restrict_impact * torch.abs(cpf_pts[:,j,i])
+                            high_impact = torch.abs(difference) > allowed_perturbation
 
-            #                if torch.sum(high_impact)!=0:
-            #                    xadv_cpf_pts[high_impact,j,i] = cpf_pts[high_impact,j,i] + allowed_perturbation[high_impact] * dx_cpf_pts[high_impact,j,i]
+                            if torch.sum(high_impact)!=0:
+                                xadv_cpf_pts[high_impact,j,i] = cpf_pts[high_impact,j,i] + allowed_perturbation[high_impact] * dx_cpf_pts[high_impact,j,i]
 
-            #for j in range(cands_per_variable['npf_pts']):
-            #    for i in range(vars_per_candidate['npf_pts']):
-            #        if i in integer_variables_by_candidate['npf_pts']:
-            #            xadv_npf_pts[:,j,i] = npf_pts[:,j,i]
-            #        else:
-            #            defaults_npf_pts = npf_pts[:,j,i].cpu() == defaults_per_variable['npf_pts'][i]
-            #            if torch.sum(defaults_npf_pts) != 0:
-            #                xadv_npf_pts[:,j,i][defaults_npf_pts] = npf_pts[:,j,i][defaults_npf_pts]
+            for j in range(cands_per_variable['npf_pts']):
+                for i in range(vars_per_candidate['npf_pts']):
+                    if i in integer_variables_by_candidate['npf_pts']:
+                        xadv_npf_pts[:,j,i] = npf_pts[:,j,i]
+                    else:
+                        defaults_npf_pts = npf_pts[:,j,i].cpu() == defaults_per_variable['npf_pts'][i]
+                        if torch.sum(defaults_npf_pts) != 0:
+                            xadv_npf_pts[:,j,i][defaults_npf_pts] = npf_pts[:,j,i][defaults_npf_pts]
 
-            #            if restrict_impact > 0:
-            #                difference = xadv_npf_pts[:,j,i] - npf_pts[:,j,i]
-            #                allowed_perturbation = restrict_impact * torch.abs(npf_pts[:,j,i])
-            #                high_impact = torch.abs(difference) > allowed_perturbation
+                        if restrict_impact > 0:
+                            difference = xadv_npf_pts[:,j,i] - npf_pts[:,j,i]
+                            allowed_perturbation = restrict_impact * torch.abs(npf_pts[:,j,i])
+                            high_impact = torch.abs(difference) > allowed_perturbation
 
-            #                if torch.sum(high_impact)!=0:
-            #                    xadv_npf_pts[high_impact,j,i] = npf_pts[high_impact,j,i] + allowed_perturbation[high_impact] * dx_npf_pts[high_impact,j,i]
+                            if torch.sum(high_impact)!=0:
+                                xadv_npf_pts[high_impact,j,i] = npf_pts[high_impact,j,i] + allowed_perturbation[high_impact] * dx_npf_pts[high_impact,j,i]
 
-            #for j in range(cands_per_variable['vtx_pts']):
-            #    for i in range(vars_per_candidate['vtx_pts']):
-            #        if i in integer_variables_by_candidate['vtx_pts']:
-            #            xadv_vtx_pts[:,j,i] = vtx_pts[:,j,i]
-            #        else:
-            #            defaults_vtx_pts = vtx_pts[:,j,i].cpu() == defaults_per_variable['vtx_pts'][i]
-            #            if torch.sum(defaults_vtx_pts) != 0:
-            #                xadv_vtx_pts[:,j,i][defaults_vtx_pts] = vtx_pts[:,j,i][defaults_vtx_pts]
+            for j in range(cands_per_variable['vtx_pts']):
+                for i in range(vars_per_candidate['vtx_pts']):
+                    if i in integer_variables_by_candidate['vtx_pts']:
+                        xadv_vtx_pts[:,j,i] = vtx_pts[:,j,i]
+                    else:
+                        defaults_vtx_pts = vtx_pts[:,j,i].cpu() == defaults_per_variable['vtx_pts'][i]
+                        if torch.sum(defaults_vtx_pts) != 0:
+                            xadv_vtx_pts[:,j,i][defaults_vtx_pts] = vtx_pts[:,j,i][defaults_vtx_pts]
 
-            #            if restrict_impact > 0:
-            #                difference = xadv_vtx_pts[:,j,i] - vtx_pts[:,j,i]
-            #                allowed_perturbation = restrict_impact * torch.abs(vtx_pts[:,j,i])
-            #                high_impact = torch.abs(difference) > allowed_perturbation
+                        if restrict_impact > 0:
+                            difference = xadv_vtx_pts[:,j,i] - vtx_pts[:,j,i]
+                            allowed_perturbation = restrict_impact * torch.abs(vtx_pts[:,j,i])
+                            high_impact = torch.abs(difference) > allowed_perturbation
 
-            #                if torch.sum(high_impact)!=0:
-            #                    xadv_vtx_pts[high_impact,j,i] = vtx_pts[high_impact,j,i] + allowed_perturbation[high_impact] * dx_vtx_pts[high_impact,j,i]
+                            if torch.sum(high_impact)!=0:
+                                xadv_vtx_pts[high_impact,j,i] = vtx_pts[high_impact,j,i] + allowed_perturbation[high_impact] * dx_vtx_pts[high_impact,j,i]
                                 
         return xadv_glob.detach(),xadv_cpf.detach(),xadv_npf.detach(),xadv_vtx.detach(),xadv_cpf_pts.detach(),xadv_npf_pts.detach(),xadv_vtx_pts.detach()

ParT/pytorch_Part.py
@@ -30,7 +30,8 @@ from definitions_ParT import epsilons_per_feature, vars_per_candidate
 glob_vars = vars_per_candidate['glob']
 
 def train_loop(dataloader, nbatches, model, loss_fn, optimizer, device, epoch, epoch_pbar, acc_loss, scaler, scheduler, attack, att_magnitude, restrict_impact, epsilon_factors):
-    for b in range(nbatches):
+    #for b in range(nbatches):
+    for b in range(2):
         
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         #should not happen unless files are broken (will give additional errors)
@@ -44,6 +45,7 @@ def train_loop(dataloader, nbatches, model, loss_fn, optimizer, device, epoch, e
         npf = torch.Tensor(features_list[2]).to(device)
         vtx = torch.Tensor(features_list[3]).to(device)
         cpf_4v = torch.Tensor(features_list[4]).to(device)
+        cpf_4v = cpf_4v[:,:,:4]
         npf_4v = torch.Tensor(features_list[5]).to(device)
         vtx_4v = torch.Tensor(features_list[6]).to(device)
         #pair = torch.Tensor(features_list[7]).to(device)
@@ -102,14 +104,15 @@ def train_loop(dataloader, nbatches, model, loss_fn, optimizer, device, epoch, e
 
         elif attack == 'FGSM':
             #print('Do FGSM')
-            glob, cpf, npf, vtx, cpf_4v, npf_4v, vtx_4v = fgsm_attack(sample=(glob,cpf,npf,vtx,cpf_4v,npf_4v,vtx_4v), 
-                                                                      epsilon=att_magnitude,
-                                                                      dev=device,
-                                                                      targets=y,
-                                                                      thismodel=model,
-                                                                      thiscriterion=loss_fn,
-                                                                      restrict_impact=restrict_impact,
-                                                                      epsilon_factors=epsilon_factors)
+            with torch.cuda.amp.autocast():
+                glob, cpf, npf, vtx, cpf_4v, npf_4v, vtx_4v = fgsm_attack(sample=(glob,cpf,npf,vtx,cpf_4v,npf_4v,vtx_4v), 
+                                                                          epsilon=att_magnitude,
+                                                                          dev=device,
+                                                                          targets=y,
+                                                                          thismodel=model,
+                                                                          thiscriterion=loss_fn,
+                                                                          restrict_impact=restrict_impact,
+                                                                          epsilon_factors=epsilon_factors)
             
         # Compute prediction and loss
         inpt = (cpf, npf, vtx, cpf_4v, npf_4v, vtx_4v)
@@ -120,7 +123,7 @@ def train_loop(dataloader, nbatches, model, loss_fn, optimizer, device, epoch, e
         optimizer.zero_grad()
         with torch.cuda.amp.autocast():
             pred = model(inpt)
-            loss = loss_fn(pred, y.type_as(pred))
+            loss = loss_fn(pred, y.type_as(pred)) # batch loss
         scaler.scale(loss).backward()
         #scaler.unscale_(optimizer)
         #torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
@@ -129,9 +132,12 @@ def train_loop(dataloader, nbatches, model, loss_fn, optimizer, device, epoch, e
         #loss.backward()
         #optimizer.step()
         #scheduler.step()
-        acc_loss += loss.detach().item()
+        acc_loss += loss.detach().item() # batch loss, is a mean over individual losses in batch
         # Update progress bar description
-        avg_loss = acc_loss / (b + 1)
+        avg_loss = acc_loss / (b + 1) # per batch, the avg_loss is updated
+        # batch 0 -> batch0_loss / 1 = batch0_loss
+        # batch 1 -> (batch0_loss + batch1_loss)/2
+        # batch 2 -> (batch0_loss + batch1_loss + batch2_loss) / 3
         #if((b % 100) == 0):
         #    print(f"Training Error: \n batch : {(b):>0.1f} / {(nbatches):>0.1f}, Avg loss: {avg_loss:>6f} \n")
         desc = f'Epoch {epoch+1} - loss {avg_loss:.6f}'
@@ -142,10 +148,11 @@ def train_loop(dataloader, nbatches, model, loss_fn, optimizer, device, epoch, e
 
 def val_loop(dataloader, nbatches, model, loss_fn, device, epoch):
     num_batches = nbatches
-    test_loss, correct = 0, 0
+    test_loss, correct, total = 0, 0, 0
  
     with torch.no_grad():
-        for b in range(nbatches):
+        #for b in range(nbatches):
+        for b in range(2):
         #should not happen unless files are broken (will give additional errors)
             #if dataloader.isEmpty():
              #   raise Exception("ran out of data") 
@@ -157,6 +164,7 @@ def val_loop(dataloader, nbatches, model, loss_fn, device, epoch):
             npf = torch.Tensor(features_list[2]).to(device)
             vtx = torch.Tensor(features_list[3]).to(device)
             cpf_4v = torch.Tensor(features_list[4]).to(device)
+            cpf_4v = cpf_4v[:,:,:4]
             npf_4v = torch.Tensor(features_list[5]).to(device)
             vtx_4v = torch.Tensor(features_list[6]).to(device)
             #pair = torch.Tensor(features_list[7]).to(device)
@@ -166,13 +174,21 @@ def val_loop(dataloader, nbatches, model, loss_fn, device, epoch):
             pred = model(inpt)
             # Compute prediction and loss
             _, labels = y.max(dim=1)
-            
-            test_loss += loss_fn(pred, y.long()).item()
+            #print(labels)
+            #print(y)
+            #total += cpf[0].shape[0] # this does not have the length of one batch, but rather the number of cpfs !
+            total += cpf.shape[0]
+            test_loss += loss_fn(pred, y.long()).item() # batch loss, added for every batch
+            avg_loss = test_loss / (b + 1)
             correct += (pred.argmax(1) == labels).type(torch.float).sum().item()
+            #print(correct)
+            #print(total)
  
-    test_loss /= num_batches
-    correct /= (num_batches * cpf[0].shape[0])
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.6f}%, Avg loss: {test_loss:>6f} \n")
+    #test_loss /= num_batches # will be total loss over all batches, divided by number of batches
+    #correct /= (num_batches * cpf[0].shape[0])
+    correct /= total # this is dividing by the total length of validation set, also works for the edge case of the last batch
+    #print(f"Test Error: \n Accuracy: {(100*correct):>0.6f}%, Avg loss: {test_loss:>6f} \n")
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.6f}%, Avg loss: {avg_loss:>6f} \n")
     return test_loss
 
 def cross_entropy_one_hot(input, target):
@@ -355,6 +371,8 @@ class training_base(object):
                 'cpf' : torch.Tensor(np.load(epsilons_per_feature['cpf']).transpose()).to(self.device),
                 'npf' : torch.Tensor(np.load(epsilons_per_feature['npf']).transpose()).to(self.device),
                 'vtx' : torch.Tensor(np.load(epsilons_per_feature['vtx']).transpose()).to(self.device),
+                #'cpf_pts' : torch.cat((torch.Tensor(np.load(epsilons_per_feature['cpf_pts']).transpose()).to(self.device)
+                #                     torch.zeros(6, device=self.device))), # more features not currently covered with attacks
                 'cpf_pts' : torch.Tensor(np.load(epsilons_per_feature['cpf_pts']).transpose()).to(self.device),
                 'npf_pts' : torch.Tensor(np.load(epsilons_per_feature['npf_pts']).transpose()).to(self.device),
                 'vtx_pts' : torch.Tensor(np.load(epsilons_per_feature['vtx_pts']).transpose()).to(self.device),
@@ -398,5 +416,6 @@ class training_base(object):
 
                     
                     self.saveModel(self.model, self.optimizer, self.trainedepoches, self.scheduler, self.best_loss, train_loss, val_loss, is_best = False)
-                
-                traingen.shuffleFileList() # Note: DJC3 shuffleFileList, older: shuffleFilelist
+                    print(self.trainedepoches, nepochs)
+                    if self.trainedepoches + 1 < nepochs: # avoiding the NoneType error at the end of training
+                        traingen.shuffleFileList() # Note: DJC3 shuffleFileList, older: shuffleFilelist